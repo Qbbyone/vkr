c844c0d8941ce41c2a9655a9d0700cc2d373929e
Polskolg <missshark.gg@gmail.com>
2024-05-01 17:45:35 +0300

    Add low-pass and high-pass filters

--- pantomkins.py
+++ pantomkins.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 
 class Pan_tompkins:
@@ -38,18 +39,89 @@ class Pan_tompkins:
         self.delay = 0
 
     def plot_raw_ecg(self):
-        print("plot raw signal")
-        # if self.fs == 200:
-        # figure,  ax(1)=subplot(321);plot(ecg);axis tight;title('Raw ECG Signal')
-        # else:
-        # figure,  ax(1)=subplot(3,2,[1 2]);plot(ecg);axis tight;title('Raw ECG Signal')
+        # print("plot raw signal")
+        if self.fs == 200:
+            plt.figure()
+            plt.plot(self.ecg)
+            plt.title("Raw ECG Signal", fontdict=None, loc="center")
+            plt.show()
+        else:
+            # todo add something to plot
+            plt.figure()
+            plt.plot(self.ecg)
+            plt.title("Raw ECG Signal", fontdict=None, loc="center")
+            plt.show()
 
-    def cancel_noise(self):
-        b = [1, 0, 0, 0, 0, 0, -2, 0, 0, 0, 0, 0, 1]
-        a = [1, -2, 1]
+    def filter_signal(self):
         print("filtering")
+        if self.fs == 200:
+            # Low Pass Filter H(z) = ((1 - z^(-6))^2)/(1 - z^(-1))^2
+            h_l = np.array([1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0, 0])
+            ecg_l = np.convolve(self.ecg, h_l)
+            ecg_l = ecg_l / np.max(np.abs(ecg_l))
+            delay = 6
+            if self.gr:
+                plt.figure()
+                plt.plot(ecg_l)
+                plt.title("Low pass filtered", fontdict=None, loc="center")
+                plt.show()
+
+            # High Pass filter H(z) = (-1+32z^(-16)+z^(-32))/(1+z^(-1))
+            h_h = np.array(
+                [
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    31,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    0,
+                ]
+            )
+            ecg_h = np.convolve(self.ecg, h_h)
+            ecg_h = ecg_h / np.max(np.abs(ecg_h))
+            delay = delay + 16  # 16 samples for highpass filtering
+            if self.gr:
+                plt.figure()
+                plt.plot(ecg_h)
+                plt.title("High Pass Filtered", fontdict=None, loc="center")
+                plt.show()
+
+        else:
+            # bandpass filter
+            # for Noise cancelation of other sampling frequencies(Filtering)
+            f1 = 5  # cuttoff low frequency to get rid of baseline wander
+            f2 = 15
+            Wn = (f1 + f2) * 2 / self.fs
+        print("filtering end")
 
     def run(self):
         self.plot_raw_ecg()
-        self.cancel_noise()
+        self.filter_signal()
         return [self.qrs_amp_raw, self.qrs_i_raw, self.delay]
